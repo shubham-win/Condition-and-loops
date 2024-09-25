e0b3ed1a59e2e35158042c3404596411a6993d04
shubham-win <shubhamp890@gmail.com>
2024-09-25 17:07:58 +0530

    Add files via upload

---
+++ Q03.js
@@ -0,0 +1,23 @@
+//  Loops - loop is a sequence of instructions that are repeated until a certain condition is reached.
+// Need -  loops allow them to shorten what could be hundreds of lines of code to just a few. 
+// This allows them to write the code once and repeat it as many times as needed, 
+// making it more likely for the program to run as expected
+
+// the different types of loops in programming, such as "For" loop, "While" loop, and "Do-While" loop.
+
+// 1. for loop example
+
+for(let i = 2; i <= 20; i+=2){
+    console.log(i);
+}
+
+
+// 2. While loop example
+
+let i = 1;
+while(i <= 25){
+    console.log(i++);
+}
+while(i >= 2){
+    console.log(--i);
+}
